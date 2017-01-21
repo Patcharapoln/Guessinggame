0d06c84a1db5f34936ac4aad71223fe7ebcaf0c1
Patcharapol <patcharapol.n@ku.th>
2017-01-21 07:52:47 +0700

    Update Main.java

--- src/Guessinggame/Main.java
+++ src/Guessinggame/Main.java
@@ -1,9 +1,10 @@
 package Guessinggame;
 
 public class Main {
+
 public static void main(String[] args) {
 	GuessingGame game = new GuessingGame(20);
 	GameConsole ui = new GameConsole();
 	ui.play(game);
-}
+	}
 }
