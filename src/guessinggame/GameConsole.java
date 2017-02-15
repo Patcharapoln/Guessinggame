cf3415f07c7f057a100879eacc603d3db627faa7
Patcharapol Nirunpornputta <patcharapol.n@ku.th>
2017-02-15 21:38:38 +0700

    Edit issue

---
+++ src/guessinggame/GameConsole.java
@@ -0,0 +1,39 @@
+package guessinggame;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Patcharapol Nirunpornputta
+ *
+ */
+public class GameConsole {
+	
+	/**
+	 * Ask users to guess a number
+	 * 
+	 * @param game
+	 * @return a secret number
+	 */
+	public int play(GuessingGame game) {
+		Scanner input = new Scanner(System.in);
+		String title = "Guessing game";
+		String prompt = "Your guess? ";
+		System.out.println(title);
+		System.out.println(game.getHint());
+		System.out.print(prompt);
+		int number = input.nextInt();
+
+		while (!game.guess(number)) {
+			System.out.println(game.getHint());
+			System.out.print(prompt);
+			number = input.nextInt();
+			game.setCount();
+		}
+		System.out.println(game.getHint());
+		System.out.println("You used " + game.getCount() + " guesses");
+		return number;
+
+	}
+
+}
---
+++ src/guessinggame/GuessingGame.java
@@ -0,0 +1,96 @@
+package guessinggame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+/**
+ * Game of guessing a secret number.
+ * 
+ * @author Patcharapol Nirunpornputta
+ */
+public class GuessingGame {
+	/* properties of a guessing game */
+	private int upperBound;
+	private int secret;
+	private String hint;
+	private int count = 1;
+	
+	/**
+	 * Initialize a new game.
+	 * 
+	 * @param upperbound
+	 *            is the max value for the secret number (>1).
+	 */
+	public GuessingGame(int upperbound) {
+		this.upperBound = upperbound;
+		this.secret = getRandomNumber(upperbound);
+		this.hint = "I'm thinking of a number between 1 and " + upperbound;
+	}
+	
+	/**
+	 * Create a random number between 1 and limit.
+	 * 
+	 * @param limit
+	 *            is the upper limit for random number
+	 * @return a random number between 1 and limit (inclusive)
+	 */
+	private int getRandomNumber(int limit) {
+		long seed = System.currentTimeMillis();
+		Random rand = new Random(seed);
+		return rand.nextInt(limit) + 1;
+	}
+
+	/**
+	 * Show a hint if user input wrong number and show secret number when user
+	 * input correct number
+	 * 
+	 * @param number
+	 *            input by user
+	 * @return
+	 */
+	public boolean guess(int number) {
+		if (number == this.secret) {
+			setHint("Correct.The secret is " + this.secret);
+			return true;
+
+		} else if (number > this.secret) {
+			setHint("Sorry you're guess is too large");
+			return false;
+		} else if (number < this.secret) {
+			setHint("Sorry you're guess is too small");
+			return false;
+		} else
+			return false;
+	}
+
+	void setHint(String hint) {
+		this.hint = hint;
+	}
+
+	/**
+	 * Return a hint based on the most recent guess.
+	 * 
+	 * @return hint based on most recent guess
+	 */
+	public String getHint() {
+		return hint;
+	}
+
+	/**
+	 * Get the number of guesses submitted.
+	 * 
+	 * @return the number of guesses
+	 */
+	public int getCount() {
+		return count;
+	}
+
+	/**
+	 * increase the number of guesses
+	 */
+	void setCount() {
+		count++;
+	}
+
+}
